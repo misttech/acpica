5eea9255c659eb8ec153da2aff5b4d28139cbd83
Robert Moore <Robert.Moore@intel.com>
2023-02-07 10:26:47 -0800

    Merge pull request #776 from t-8ch/dangling-pointer
    Fix dangling pointer warning for AcpiUtInitStackPtrTrace
    
    GitOrigin-RevId: 8e305d43ea7c80df310647a29b7609aeba57085d

--- source/include/platform/aclinux.h
+++ source/include/platform/aclinux.h
@@ -64,6 +64,8 @@
 #define ACPI_USE_DO_WHILE_0
 #define ACPI_IGNORE_PACKAGE_RESOLUTION_ERRORS
 
+#define ACPI_UINTPTR_T uintptr_t
+
 
 #ifdef __KERNEL__
 
@@ -148,8 +150,6 @@
 #define ACPI_SPINLOCK               spinlock_t *
 #define ACPI_CPU_FLAGS              unsigned long
 
-#define ACPI_UINTPTR_T              uintptr_t
-
 #define ACPI_TO_INTEGER(p)          ((uintptr_t)(p))
 #define ACPI_OFFSET(d, f)           offsetof(d, f)
 
@@ -207,6 +207,7 @@
 
 #ifdef ACPI_USE_STANDARD_HEADERS
 #include <unistd.h>
+#include <stdint.h>
 #endif
 
 /* Define/disable kernel-specific declarators */
