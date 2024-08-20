0f69137029d5c76e020efc715e3aca70cbc01ace
Saket Dumbre <saket.dumbre@intel.com>
2024-08-20 15:56:42 -0700

    Merge pull request #964 from alexVinarskis/pr/fix-iasl-segfault
    iasl: handle empty ConnectionNode
    GitOrigin-RevId: 499b076c9430777d6c0dc46da0f7fe042c85a0e1

--- source/components/executer/exprep.c
+++ source/components/executer/exprep.c
@@ -507,6 +507,10 @@ AcpiExPrepFieldValue (
         if (Info->ConnectionNode)
         {
             SecondDesc = Info->ConnectionNode->Object;
+            if (SecondDesc == NULL)
+            {
+                break;
+            }
             if (!(SecondDesc->Common.Flags & AOPOBJ_DATA_VALID))
             {
                 Status = AcpiDsGetBufferArguments (SecondDesc);
